2a5022805bfb20ebcccde78a79456afb060aac9c
Alexandra <sasha.dobryakova@gmail.com>
2022-11-08 12:54:01 +0500

    changed spider and fly spawn

--- Assets/Scripts/Model/GameFlow/CreateEnemiesManager.cs
+++ Assets/Scripts/Model/GameFlow/CreateEnemiesManager.cs
@@ -18,13 +18,17 @@ public class CreateEnemiesManager : MonoBehaviour
     [SerializeField] private int spiderLayer = 9;
     [SerializeField] private int lizardLayer = 8;
     [SerializeField] private int flyLayer = 7;
+    [SerializeField] private int playerLayer = 3;
 
     private List<Vector2> _raycastPoints1 = new();
     private List<Vector2> _raycastPoints2 = new();
     private List<Vector2> _raycastPoints3 = new();
-    
-    private int _currentLayer;
+
+    private int _spawnerLayer;
     private bool _isThisFly;
+    private bool _isThisSpider;
+
+    private List<Vector2> _directionsForSpawnSpider = new();
 
     [Serializable]
     public struct EnemiesCreating
@@ -43,6 +47,11 @@ public class CreateEnemiesManager : MonoBehaviour
     private void Start()
     {
         StartCreateEnemies();
+
+        _directionsForSpawnSpider.Add(Vector2.down);
+        _directionsForSpawnSpider.Add(Vector2.up);
+        _directionsForSpawnSpider.Add(Vector2.left);
+        _directionsForSpawnSpider.Add(Vector2.right);
     }
 
     private void StartCreateEnemies()
@@ -58,6 +67,7 @@ public class CreateEnemiesManager : MonoBehaviour
         {
             yield return new WaitForSeconds(creatingEnemies[0].timeToCreate);
             _isThisFly = false;
+            _isThisSpider = true;
             Vector3 positionOfNewEnemy = GetPointForSpawn(10, 0);
             Instantiate(spiderPrefab, positionOfNewEnemy, Quaternion.identity);
         }
@@ -69,6 +79,7 @@ public class CreateEnemiesManager : MonoBehaviour
         {
             yield return new WaitForSeconds(creatingEnemies[1].timeToCreate);
             _isThisFly = false;
+            _isThisSpider = false;
             Vector3 positionOfNewEnemy = GetPointForSpawn(10, 1);
             Instantiate(lizardPrefab, positionOfNewEnemy, Quaternion.identity);
         }
@@ -80,6 +91,7 @@ public class CreateEnemiesManager : MonoBehaviour
         {
             yield return new WaitForSeconds(creatingEnemies[2].timeToCreate);
             _isThisFly = true;
+            _isThisSpider = false;
             Vector3 positionOfNewEnemy = GetPointForSpawn(10, 2);
             Instantiate(flyPrefab, positionOfNewEnemy, Quaternion.identity);
         }
@@ -95,8 +107,26 @@ public class CreateEnemiesManager : MonoBehaviour
             var posOfPointY = Random.Range(point2.position.y, point1.position.y);
             Vector2 positionOfRandomPoint = new Vector2(posOfPointX, posOfPointY);
 
+
             // raycast
-            if (_isThisFly == false) // if a spider or a lizard
+            if (_isThisSpider) // if a spider 
+            {
+                int randomDirectionForSpawnSpider = Random.Range(0, 4);
+
+                RaycastHit2D hit = Physics2D.Raycast(positionOfRandomPoint,
+                    _directionsForSpawnSpider[randomDirectionForSpawnSpider], 1,
+                    LayerMask.GetMask("Level"));
+                if (hit.collider == null)
+                    raycastPoints.Add(positionOfRandomPoint);
+            }
+            else if (_isThisFly) // if a fly 
+            {
+                RaycastHit2D hit = Physics2D.Raycast(positionOfRandomPoint, new Vector2(0, -1), 1,
+                    LayerMask.GetMask("Level"));
+                if (hit.collider == null)
+                    raycastPoints.Add(positionOfRandomPoint);
+            }
+            else // if a lizard 
             {
                 RaycastHit2D hit = Physics2D.Raycast(positionOfRandomPoint, new Vector2(0, -1), 100,
                     LayerMask.GetMask("Level"));
@@ -105,18 +135,10 @@ public class CreateEnemiesManager : MonoBehaviour
                     raycastPoints.Add(hit.point);
                 }
             }
-            else // if a fly 
-            {
-                RaycastHit2D hit = Physics2D.Raycast(positionOfRandomPoint, new Vector2(0, -1), 1,
-                    LayerMask.GetMask("Level"));
-                if (hit.collider == null)
-                {
-                    raycastPoints.Add(positionOfRandomPoint);
-                }
-            }
         }
     }
-    
+
+
     private Vector2 GetPointForSpawn(int countOfRaycasts, int numOfLayer)
     {
         var result = new List<Vector2>();
@@ -125,19 +147,19 @@ public class CreateEnemiesManager : MonoBehaviour
         {
             case 0:
                 _raycastPoints1.Clear();
-                _currentLayer = spiderLayer;
+                _spawnerLayer = playerLayer;
                 GetRaycastPoints(countOfRaycasts, _raycastPoints1);
                 result = SortList(_raycastPoints1);
                 break;
             case 1:
                 _raycastPoints2.Clear();
-                _currentLayer = lizardLayer;
+                _spawnerLayer = lizardLayer;
                 GetRaycastPoints(countOfRaycasts, _raycastPoints2);
                 result = SortList(_raycastPoints2);
                 break;
             case 2:
                 _raycastPoints3.Clear();
-                _currentLayer = flyLayer;
+                _spawnerLayer = flyLayer;
                 GetRaycastPoints(countOfRaycasts, _raycastPoints3);
                 result = SortList(_raycastPoints3);
                 break;
@@ -148,10 +170,27 @@ public class CreateEnemiesManager : MonoBehaviour
 
     private List<Vector2> SortList(List<Vector2> raycastPoints)
     {
+        if (raycastPoints.Count < 2)
+            return raycastPoints;
+
         raycastPoints.Sort((a, b) =>
             {
-                var vectorToFirst = (Vector2)Physics2D.OverlapCircle(a, 100, _currentLayer).transform.position - a;
-                var vectorToSecond = (Vector2)Physics2D.OverlapCircle(b, 100, _currentLayer).transform.position - b;
+                var firstSpawnerInRadius = Physics2D.OverlapCircle(a, 100, _spawnerLayer);
+                var secondSpawnerInRadius = Physics2D.OverlapCircle(b, 100, _spawnerLayer);
+
+                if (firstSpawnerInRadius == null && secondSpawnerInRadius == null
+                    || firstSpawnerInRadius != null && secondSpawnerInRadius == null)
+                {
+                    return 1;
+                }
+
+                if (firstSpawnerInRadius == null && secondSpawnerInRadius != null)
+                {
+                    return -1;
+                }
+
+                Vector2 vectorToFirst = (Vector2)firstSpawnerInRadius.transform.position - a;
+                Vector2 vectorToSecond = (Vector2)secondSpawnerInRadius.transform.position - b;
 
                 return vectorToSecond.magnitude.CompareTo(vectorToFirst.magnitude);
             }
